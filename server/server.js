b2407dfaad646c565e6eb43ffecaea336eaf03e5
Marcio Nicolau <nicolau.3g@gmail.com>
2019-02-14 01:16:12 -0200

    Add front- and back-end

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb://root:example@localhost:27017/nodeapi?authSource=admin",
+  {
+    useNewUrlParser: true
+  }
+);
+
+requireDir("./src/models");
+
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
